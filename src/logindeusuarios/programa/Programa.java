7feb2d2514fda2aaf3e29d4f1fbc6869e1ac4bae
Walter <walter.filho@venturus.org.br>
2018-05-03 18:09:32 -0300

    mudancas de porta e identacao

--- src/logindeusuarios/programa/Programa.java
+++ src/logindeusuarios/programa/Programa.java
@@ -1,11 +1,5 @@
 package logindeusuarios.programa;
 
-import java.awt.FlowLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import logindeusuarios.bd.*;
-import logindeusuarios.bd.dbos.*;
-
 public class Programa
 {
     public static void main(String[] args)
--- src/logindeusuarios/socket/Receptora.java
+++ src/logindeusuarios/socket/Receptora.java
@@ -13,29 +13,29 @@ import java.net.*;
 public class Receptora 
 {
 
-	public static void main(String[] args) 
-        {
-		
-		try{
-			//*****RECEPTORA*****
-			ServerSocket pedido = new ServerSocket(2222);
-			Socket conexao = pedido.accept(); //accept retorna o socket da transmissora											//por isso n„o È instanciada
-			ObjectInputStream receptor = 
-			new ObjectInputStream(conexao.getInputStream());
-			
-			Solicitacao recebido;
-			do
-			{
-			    recebido = (Solicitacao) receptor.readObject();
-			}										
-			while(recebido.getComando().equals("FIM"));										
-		}
-		catch (IOException | ClassNotFoundException erro)
-		{
-			System.err.println(erro.getMessage());
-		}
+    public static void main(String[] args) 
+    {
 
-	}
+        try{
+                //*****RECEPTORA*****
+                ServerSocket pedido = new ServerSocket(33333);
+                Socket conexao = pedido.accept();											//por isso n„o È instanciada
+                ObjectInputStream receptor = 
+                new ObjectInputStream(conexao.getInputStream());
+
+                Solicitacao recebido;
+                do
+                {
+                    recebido = (Solicitacao) receptor.readObject();
+                }										
+                while(recebido.getComando().equals("FIM"));										
+        }
+        catch (IOException | ClassNotFoundException erro)
+        {
+                System.err.println(erro.getMessage());
+        }
+
+    }
 
 }
 
